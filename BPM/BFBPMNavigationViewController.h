9b0d1ecdd01c010e23b3ae24dafb06264a64e938
Christian Andersen <christian@christianandersen.com>
2019-05-31 15:24:52 +1000

    add view controllers

---
+++ BPM/BFBPMNavigationViewController.h
@@ -0,0 +1,17 @@
+//
+//  BFBPMNavigationViewController.h
+//  BPM
+//
+//  Created by Christian Andersen on 31/5/19.
+//  Copyright © 2019 Christian Andersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BFBPMNavigationViewController : UINavigationController
+
+@end
+
+NS_ASSUME_NONNULL_END
